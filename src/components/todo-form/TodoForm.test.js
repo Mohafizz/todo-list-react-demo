d124e90987872e6eda4c2081cc898e4f54af8fb8
mohafizz <mohafizz@gmail.com>
2018-03-22 21:40:59 +0800

    passing tests for TodoForm without onChange

--- src/components/todo-form/TodoForm.test.js
+++ src/components/todo-form/TodoForm.test.js
@@ -4,6 +4,13 @@ import TodoForm from "./TodoForm";
 import TodoList from "../todo-list/TodoList";
 
 describe("TodoForm ", () => {
+  it("should have the list of following html tags", () => {
+    const wrapper = shallow(<TodoForm />);
+    expect(wrapper.find("div")).toHaveLength(1);
+    expect(wrapper.find("label")).toHaveLength(1);
+    expect(wrapper.find("input#form-input").props().type).toBe("text");
+  });
+
   it("should handle submit form properly", () => {
     const mockHandler = jest.fn();
     const wrapper = shallow(<TodoForm submitForm={mockHandler} />);
@@ -15,6 +22,6 @@ describe("TodoForm ", () => {
   it("should handle input for newtodo item", () => {
     const newtodoDescription = "testing";
     const wrapper = shallow(<TodoForm value={newtodoDescription} />);
-    expect(wrapper.find("input")).toHaveLength(2);
+    expect(wrapper.find("input#form-input").props().value).toBe("testing");
   });
 });

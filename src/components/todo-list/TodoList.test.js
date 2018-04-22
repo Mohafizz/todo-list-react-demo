a7bea35f828842af742615099dcd8f5e442e9591
mohafizz <mohafizz@gmail.com>
2018-04-22 20:20:30 +0800

    added snapshot testing and updated react-test-renderer to resolve problem with tree.toTree()

--- src/components/todo-list/TodoList.test.js
+++ src/components/todo-list/TodoList.test.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { shallow } from "enzyme";
+import renderer from "react-test-renderer";
 import TodoList from "./TodoList";
 import TodoItem from "../todo-item/TodoItem";
 import TodoForm from "../todo-form/TodoForm";
 
+const wrapper = shallow(<TodoList title="todos" />);
+
 describe("TodoList ", () => {
   it("should render TodoList properly", () => {
-    const wrapper = shallow(<TodoList title="todos" />);
     expect(wrapper.find("h1").props().children).toBe("todos");
     expect(wrapper.find("#todo-list")).toHaveLength(1);
     expect(wrapper.find("h1#todo-title")).toHaveLength(1);
@@ -14,19 +16,11 @@ describe("TodoList ", () => {
     expect(wrapper.find(TodoForm)).toHaveLength(1);
   });
 
-  it.skip("should add a new todo when taskCompleted method is called", () => {
-    const newTodo = { description: "Test", isCompleted: false };
-    const wrapper = shallow(<TodoList />);
-    const expectedLength = wrapper.state().todos.length + 1;
-
-    wrapper.find("TodoItem").length;
-    console.log(
-      wrapper
-        .find("TodoItem")
-        .props()
-        .done(newTodo)
-    );
-
-    expect(wrapper.state().todos).toHaveLength(expectedLength);
+  it("should match the snapshot", () => {
+    const tree = renderer.create(<TodoList />);
+    console.log("tree.toJSON(): ", tree.toJSON());
+    console.log("tree.toTree(): ", tree.toTree());
+    console.log("wrapper: ", wrapper.debug());
+    expect(tree.toJSON()).toMatchSnapshot();
   });
 });
